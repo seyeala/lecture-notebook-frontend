f993488657f87cd552107771e7431b60789e8186
seyeala <133924929+seyeala@users.noreply.github.com>
2026-09-18 22:06:43 -0600

    CP-3: avoid formatter-sensitive initializer layout

--- app/lib/services/lecture_capture.dart
+++ app/lib/services/lecture_capture.dart
@@ -5,9 +5,11 @@ import 'package:butterfly/models/lecture_capture.dart';
 import 'package:butterfly_api/butterfly_api.dart';
 
 class LectureCaptureService {
-  LectureCaptureService({DateTime Function()? clock}) : _clock = clock ?? DateTime.now;
+  LectureCaptureService({DateTime Function()? clock}) {
+    _clock = clock ?? DateTime.now;
+  }
 
-  final DateTime Function() _clock;
+  late final DateTime Function() _clock;
   String? _sessionId;
   DateTime? _createdAt;
   String? _activePageId;
