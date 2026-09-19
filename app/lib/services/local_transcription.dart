82d4bf455eae50c325dc306be57808a8d32851ef
seyeala <133924929+seyeala@users.noreply.github.com>
2026-09-18 21:58:44 -0600

    CP-4: add runtime-neutral local transcription interface

---
+++ app/lib/services/local_transcription.dart
@@ -0,0 +1,6 @@
+import 'package:butterfly/models/audio_capture.dart';
+import 'package:butterfly/models/local_transcript.dart';
+
+abstract class LocalTranscriptionService {
+  Future<LocalTranscript> transcribe(AudioCaptureArtifact audio);
+}
