7a646f983c6f450f92a3234417b4aa912223a892
seyeala <133924929+seyeala@users.noreply.github.com>
2026-09-18 22:04:29 -0600

    CP-4: add transcription service disposal contract

--- app/lib/services/local_transcription.dart
+++ app/lib/services/local_transcription.dart
@@ -3,4 +3,5 @@ import 'package:butterfly/models/local_transcript.dart';
 
 abstract class LocalTranscriptionService {
   Future<LocalTranscript> transcribe(AudioCaptureArtifact audio);
+  void dispose();
 }
